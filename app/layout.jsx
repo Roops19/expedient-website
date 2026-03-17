04b1703ceac184dd121a9aa393d87e6e685f754c
Roops19 <roopalikhurana19@gmail.com>
2026-03-17 14:08:18 +0100

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Expedient",
+  description: "Procurement & Commercial Transformation",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
